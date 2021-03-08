86ceaefa28343b6fc37a5f6733aff4ca5a8adaf9
hangyulin <lin.john@hotmail.com>
2021-03-08 00:00:53 -0500

    finally

--- Lab 2/new_clock.py
+++ Lab 2/new_clock.py
@@ -39,8 +39,8 @@ disp2 = st7789.ST7789(
     baudrate=BAUDRATE,
     width=135,
     height=240,
-    x_offset=53,
-    y_offset=100,
+    x_offset=140,
+    y_offset=40,
 )
 
 # Create blank image for drawing.
