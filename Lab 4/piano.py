809ceec1572dc2dded48afd90b22a94bfb18c572
hangyulin <lin.john@hotmail.com>
2021-04-04 23:24:12 -0400

    done

--- Lab 4/piano.py
+++ Lab 4/piano.py
@@ -5,6 +5,11 @@ import board
 import busio
 import adafruit_ssd1306
 import pygame
+import adafruit_mpr121
+from collections import deque
+from PIL import Image, ImageDraw, ImageFont
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+mpr121 = adafruit_mpr121.MPR121(i2c)
 
 # Create the I2C interface.
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -13,83 +18,41 @@ i2c = busio.I2C(board.SCL, board.SDA)
 # The first two parameters are the pixel width and pixel height.  Change these
 # to the right size for your display!
 oled = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+image = Image.new("1", (oled.width, oled.height))
+draw = ImageDraw.Draw(image)
 
-def play_c():
+music_script = deque([3,3,3,3,5,1,2,3])
+
+def play_key(key):
     pygame.mixer.init()
-    pygame.mixer.music.load("/Developing and Designing Interactive Devices/Interactive-Lab-Hub/Lab 4/sound/g.mp3")
+    if key == 1:
+        pygame.mixer.music.load("/Developing and Designing Interactive Devices/Interactive-Lab-Hub/Lab 4/sound/c.mp3")
+    elif key == 2:
+        pygame.mixer.music.load("/Developing and Designing Interactive Devices/Interactive-Lab-Hub/Lab 4/sound/d.mp3")
+    elif key == 3:
+        pygame.mixer.music.load("/Developing and Designing Interactive Devices/Interactive-Lab-Hub/Lab 4/sound/e.mp3")
+    elif key == 4:
+        pygame.mixer.music.load("/Developing and Designing Interactive Devices/Interactive-Lab-Hub/Lab 4/sound/f.mp3")
+    else:
+        pygame.mixer.music.load("/Developing and Designing Interactive Devices/Interactive-Lab-Hub/Lab 4/sound/g.mp3")
     pygame.mixer.music.play()
     while pygame.mixer.music.get_busy() == True:
         continue
-
-# Helper function to draw a circle from a given position with a given radius
-# This is an implementation of the midpoint circle algorithm,
-# see https://en.wikipedia.org/wiki/Midpoint_circle_algorithm#C_example for details
-def draw_circle(xpos0, ypos0, rad, col=1):
-    x = rad - 1
-    y = 0
-    dx = 1
-    dy = 1
-    err = dx - (rad << 1)
-    while x >= y:
-        oled.pixel(xpos0 + x, ypos0 + y, col)
-        oled.pixel(xpos0 + y, ypos0 + x, col)
-        oled.pixel(xpos0 - y, ypos0 + x, col)
-        oled.pixel(xpos0 - x, ypos0 + y, col)
-        oled.pixel(xpos0 - x, ypos0 - y, col)
-        oled.pixel(xpos0 - y, ypos0 - x, col)
-        oled.pixel(xpos0 + y, ypos0 - x, col)
-        oled.pixel(xpos0 + x, ypos0 - y, col)
-        if err <= 0:
-            y += 1
-            err += dy
-            dy += 2
-        if err > 0:
-            x -= 1
-            dx += 2
-            err += dx - (rad << 1)
-
-
-# initial center of the circle
-center_x = 63
-center_y = 15
-# how fast does it move in each direction
-x_inc = 1
-y_inc = 1
-# what is the starting radius of the circle
-radius = 8
 
 # start with a blank screen
 oled.fill(0)
 # we just blanked the framebuffer. to push the framebuffer onto the display, we call show()
 oled.show()
 while True:
-    # undraw the previous circle
-    draw_circle(center_x, center_y, radius, col=0)
+    if not music_script:
+        next_text = 'DONE'
+    else:
+        next_text = 'Play ' + str(music_script[0])
 
-    # if bouncing off right
-    if center_x + radius >= oled.width:
-        # start moving to the left
-        x_inc = -1
-    # if bouncing off left
-    elif center_x - radius < 0:
-        # start moving to the right
-        x_inc = 1
+    draw.text((0, 0), next_text, font=font, fill="#0000FF")
 
-    # if bouncing off top
-    if center_y + radius >= oled.height:
-        # start moving down
-        y_inc = -1
-    # if bouncing off bottom
-    elif center_y - radius < 0:
-        # start moving up
-        y_inc = 1
-
-    # go more in the current direction
-    center_x += x_inc
-    center_y += y_inc
-
-    # draw the new circle
-    draw_circle(center_x, center_y, radius)
     # show all the changes we just made
     oled.show()
-    play_c()
+    if music_script and mpr121[music_script[0]].value:
+        play_key(music_script[0])
+        music_script.popleft()
